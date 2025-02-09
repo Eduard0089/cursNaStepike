2ef2454bab565d3b9aaeeed55e3b092aa95a872b
Eduard0089 <erahmaev@mail.ru>
2025-02-09 21:12:06 +0500

    1

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public static double integrate(DoubleUnaryOperator f, double a, double b) {
-    double n = 1e-6;
-    double sum = 0;
-    long i = 0;
-    for (double x = a; x < b; x = a + n * i++) {
-        sum += n * f.applyAsDouble(x);
-    }
-    return sum;
-}
