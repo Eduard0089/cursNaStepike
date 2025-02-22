2dd5fbcaa7a8436f1d7f2e9c10ba54644b33b050
Eduard0089 <erahmaev@mail.ru>
2025-02-23 03:19:30 +0500

    1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,39 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        StringBuilder sb = new StringBuilder();
+        String line;
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            while ((line = reader.readLine()) != null && !line.isEmpty()) {
+                sb.append(line).append(" ");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
+
+        Stream<String> stream = Arrays.stream(sb.toString().split("[^a-zA-Zа-яА-Я0-9']+"))
+                .filter(w -> !w.isEmpty())
+                .map(String::toLowerCase);
+
+        Map<String, Long> countOfWords = stream.collect(groupingBy(identity(), counting()));
+
+        List<Map.Entry<String, Long>> sortedEntries = countOfWords.entrySet().stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed()
+                        .thenComparing(Map.Entry.comparingByKey()))
+                .limit(10)
+                .collect(Collectors.toList());
+
+        sortedEntries.forEach(entry -> System.out.println(entry.getKey()));
     }
 }
